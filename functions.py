1bbaaf031e11e9cfe83bd07f0c6a6fc47993f005
unknown <machel.s.kassah@gmail.com>
2018-01-24 20:01:58 +0000

    Initial build of app

---
+++ functions.py
@@ -0,0 +1,16 @@
+def add ( num1, num2):
+    sum =  num1 + num2
+    return sum
+
+def multiply (numA, numB):
+    product = numa*numb
+    return product
+
+def subtract (bigNum , smallNum):
+    difference = bigNum - smallNum
+    return difference
+
+def divide (a, b):
+    res = a/b
+    return res
+
